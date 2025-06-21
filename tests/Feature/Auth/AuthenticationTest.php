87b06ea4f3fe06f2ea703af847fe275e55f8f30f
Dip Ghosh <dipnsucse@gmail.com>
2025-06-21 22:20:58 +0600

    new change space remove

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -14,14 +14,12 @@ class AuthenticationTest extends TestCase
     public function test_login_screen_can_be_rendered(): void
     {
         $response = $this->get('/login');
-
         $response->assertStatus(200);
     }
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
         $user = User::factory()->create();
-
         $response = $this->post('/login', [
             'email'    => $user->email,
             'password' => 'password',
@@ -34,7 +32,6 @@ class AuthenticationTest extends TestCase
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
         $user = User::factory()->create();
-
         $this->post('/login', [
             'email'    => $user->email,
             'password' => 'wrong-password',
